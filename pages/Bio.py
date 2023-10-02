37e541ff5de739b9df786b2584aee5da3c62ac69
naa-7 <nalawad000@citymail.cuny.edu>
2023-10-01 23:57:07 -0400

    update profile image path

--- pages/Bio.py
+++ pages/Bio.py
@@ -8,16 +8,11 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to our portfolios")
+st.write("# Welcome to DSNOA portfolio")
 
-col1, col2 = st.columns([1, 1])
+st.image("https://avatars.githubusercontent.com/u/146592612?s=200&v=4")
 
-with col1:
-    st.image('https://avatars.githubusercontent.com/u/44613678?v=4')
-with col2:
-    st.image('https://media.licdn.com/dms/image/D4E03AQGRSiRBWejC3w/profile-displayphoto-shrink_800_800/0/1692311569649?e=1701907200&v=beta&t=GKG81ijHzFa9BSPDszOoUpQEY0OmPHogny_w1_N5oZg')
-
-important_links = '''
+important_links = """
 
 [Company Github](https://github.com/NOA2023) _as a main contributors_.,
 
@@ -29,6 +24,6 @@ Github:
 Linkedin:
 [naa7](https://www.linkedin.com/in/naa7/),
 [omr50](https://www.linkedin.com/in/omr-abdelhamed/)
-'''
+"""
 
 st.markdown(important_links)
